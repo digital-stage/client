1d3e7069ab9eefbc347f54d1f8459053d07f38df
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-04-27 22:16:02 +0200

    Current State

--- src/digitalstage/ov/OvHandler.cpp
+++ src/digitalstage/ov/OvHandler.cpp
@@ -7,6 +7,7 @@
 #include <eventpp/utilities/argumentadapter.h>
 #include <iostream>
 #include <memory>
+#include <nlohmann/json.hpp>
 
 using namespace DigitalStage;
 using namespace DigitalStage::Types;
@@ -26,27 +27,54 @@ void OvHandler::init()
       });
 
   client->appendListener(
+      EventType::SOUND_CARD_ADDED,
+      eventpp::argumentAdapter(
+          std::function<void(const EventSoundCardAdded&, const Store&)>(
+              [&](const EventSoundCardAdded& e, const Store& s) {
+                auto soundCardId = e.getSoundCard().uuid;
+                if( soundCardId == "default" ) {
+                  // The sound card
+                  auto localDevice = s.getLocalDevice();
+                  if(localDevice && localDevice->soundCardId != soundCardId) {
+                    nlohmann::json update;
+                    update["_id"] = localDevice->_id;
+                    update["soundCardId"] = e.getSoundCard().uuid;
+                    client->send("change-device", update.dump());
+                  }
+                  //TODO: Handle input and output channels
+                }
+              })));
+
+  client->appendListener(
+      EventType::SOUND_CARD_CHANGED,
+      eventpp::argumentAdapter(
+          std::function<void(const EventSoundCardChanged&, const Store&)>(
+              [&](const EventSoundCardChanged& e, const Store& s) {
+                auto soundCard = s.getSoundCard(e.getId());
+                if( soundCard && soundCard->uuid == "default" ) {
+                  //TODO: Handle input and output channels
+                }
+              })));
+
+  client->appendListener(
       EventType::LOCAL_DEVICE_READY,
       eventpp::argumentAdapter(
           std::function<void(const EventLocalDeviceReady&, const Store&)>(
               [&](const EventLocalDeviceReady&, const Store&) {
-                auto inputPorts = controller->getInputPorts();
-                auto outputPorts = controller->getInputPorts();
+                double sampleRate = controller->getSampleRate();
                 soundcard_t soundCard;
                 soundCard.uuid = "default";
+                soundCard.label = "Jack";
                 soundCard.drivers.emplace_back("JACK");
                 soundCard.driver = "JACK";
-                soundCard.inputChannels = inputPorts;
-                soundCard.inputChannels = outputPorts;
-
-                std::cout << "TODO: SEND INPUTS" << std::endl;
-                for(const std::string& el : inputPorts) {
-                  std::cout << el << std::endl;
-                }
-                std::cout << "TODO: SEND OUTPUTS" << std::endl;
-                for(const std::string& el : outputPorts) {
-                  std::cout << el << std::endl;
-                }
+                soundCard.inputChannels = controller->getInputPorts();
+                soundCard.outputChannels = controller->getInputPorts();
+                soundCard.isDefault = true;
+                soundCard.periodSize = controller->getBufferSize();
+                soundCard.numPeriods = 2;
+                soundCard.sampleRate = sampleRate;
+                soundCard.sampleRates = {sampleRate};
+                client->send("set-sound-card", (nlohmann::json) soundCard);
               })));
 }
 
